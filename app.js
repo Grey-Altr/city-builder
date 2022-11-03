035b99f2ed8207438f7aa5822ca2341eec8a34e7
Steven Bryant <stevelad_83@yahoo.com>
2022-11-02 20:07:11 -0400

    grabbing dom elements and setting initial state

--- app.js
+++ app.js
@@ -2,8 +2,19 @@
 
 /* Get DOM Elements */
 
-/* State */
+const climateDrop = document.getElementById('climate-drop');
+const architectureDrop = document.getElementById('architecture-drop');
+const nightLifeDrop = document.getElementById('nightlife-drop');
 
+const sloganEl = document.querySelector('#slogans');
+const sloganInput = document.querySelector('#slogan-input');
+const sloganBtn = document.getElementById('slogan-input-button');
+/* State */
+let climateCounter = 0;
+let architectureCounter = 0;
+let nightlifeTracker = 0;
+
+let slogans = [];
 /* Events */
 
 /* Display Functions */
