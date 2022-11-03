73b8aea7e83160af888284a15ccb440bf7546f3c
Steven Bryant <stevelad_83@yahoo.com>
2022-11-03 17:42:15 -0400

    adding slogan event listener

--- app.js
+++ app.js
@@ -26,20 +26,41 @@ climateDrop.addEventListener('change', (e) => {
     const value = e.target.value;
     climateImg.src = `./assets/${value}.jpeg`;
     climateCounter++;
+    displayCounter();
 });
 
 architectureDrop.addEventListener('change', (e) => {
     const value = e.target.value;
     architectureImg.src = `./assets/${value}.jpeg`;
     architectureCounter++;
+    displayCounter();
 });
 
 nightLifeDrop.addEventListener('change', (e) => {
     const value = e.target.value;
     nightLifeImg.src = `./assets/${value}.jpeg`;
     nightLifeCounter++;
+    displayCounter();
 });
-/* Display Functions */
 
+sloganBtn.addEventListener('click', (e) => {
+    const value = e.target.value;
+    slogans.push(sloganInput.value);
+    sloganInput.value = '';
+    displaySlogans();
+});
+
+/* Display Functions */
+function displayCounter() {
+    changeCounter.innerText = `You have changed climate ${climateCounter} times, architecture ${architectureCounter} times, and nightlife ${nightLifeCounter} times.`;
+}
+
+function displaySlogans() {
+    sloganEl.innerText = '';
+    for (let slogan of slogans) {
+        const p = document.createElement('p');
+        p.textContent = slogan;
+        sloganEl.append(p);
+    }
+}
 // (don't forget to call any display functions you want to run on page load!)
-test;
